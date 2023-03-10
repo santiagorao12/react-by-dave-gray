ee01fba68bbb07e6559ec5418a73765e44ec4900
santiago <santiagorao43@gmail.com>
2023-03-10 20:34:44 +0800

    added the names in content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import header from './header'
+import Header from './Header';
+import Content from './Content';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hi</h1>
-      </header>
-    </div>
+     <Header />
+     <Content />
+     </div>
   );
 }
 
